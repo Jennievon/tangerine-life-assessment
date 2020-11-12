6bc5dfe19273afb290979ed82d92b9d1ac78c58a
Jennifer Echenim <floxzyjay@gmail.com>
2020-11-12 21:36:48 +0100

    UI: column mappings view

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
 // function gotoTable() {
 //     window.location.href = '/pages/columns.html';
 // }
+
+var tableArray = '/assets/table.json'
+
+function tableArray() {
+    console.log('tableArray>>>>>>', tableArray);
+}
